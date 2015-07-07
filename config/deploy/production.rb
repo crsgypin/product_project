cd6e10a22719fd6f4883de3c082921b6ab44d54c
ChiuGypin <crsgypin@gmail.com>
2015-07-07 21:48:34 +0800

    update deploy.rb and production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 # You can define all roles on a single server, or split them:
 set :stage, :production
 
-server '139.162.11.161', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+server '139.162.11.161', user: 'gypin', roles: %w{app db web}, my_property: :my_value
 # server '139.162.11.161', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.139.162.11.161', user: 'deploy', roles: %w{db}
 
