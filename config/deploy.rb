784b7e26485f548993e353f18b20b40e4015e350
ChiuGypin <crsgypin@gmail.com>
2015-07-07 19:09:01 +0800

    Add desc in deploy.rb file

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,6 +41,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache',
 
 namespace :deploy do
 
+	desc 'Restart application'
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
